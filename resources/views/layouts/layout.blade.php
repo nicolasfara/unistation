33d62c92c430aba8b004fb2876fda2450bcaaa79
CavallucciMartina <martina.cavallucci96@gmail.com>
2019-05-02 19:17:35 +0200

    Add materialize css

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="it">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  @stack('styles')
+</head>
+
+<body>
+  @yield('content')
+
+  @stack('scripts')
+</body>
+</html>
