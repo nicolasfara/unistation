fa2087c284d7ce833722220f561b237ebf47cdd2
nic0lasfara <nicolas.farabegoli@gmail.com>
2019-05-04 15:43:03 +0200

    Fixed navbar with menu hamburger icon and import materialize JS

---
+++ resources/js/nav.js
@@ -0,0 +1,11 @@
+/*
+ * nav.js
+ * Copyright (C) 2019 nicolasfara <nicolas.farabegoli@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {  });
+});
