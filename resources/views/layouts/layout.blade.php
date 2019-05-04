4ab706620eda934ff5a77e718097dcaa3d746d28
CavallucciMartina <martina.cavallucci96@gmail.com>
2019-05-04 15:19:22 +0200

    Fix csrf-token error

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="_token" content="{{ csrf_token() }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', config('app.name') )</title>
     <meta name="description" content="The HTML5 Herald">
     <meta name="author" content="SitePoint">
