ea36365a2c0b7ecf7e35dafcd849a4d830056eef
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2019-05-04 11:08:57 +0200

    Fix footer on bottom and add CSS for footer

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,22 +1,24 @@
 <!doctype html>
 <html lang="it">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="_token" content="{{ csrf_token() }}" />
-  <title>@yield('title', config('app.name') )</title>
-  <meta name="description" content="The HTML5 Herald">
-  <meta name="author" content="SitePoint">
-  @yield('meta')
-  @stack('styles')
-</head>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="_token" content="{{ csrf_token() }}" />
+    <title>@yield('title', config('app.name') )</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+    @yield('meta')
+    @stack('styles')
+  </head>
 
-<body>
-  @include('includes.nav')
- <!-- @include('includes.partials.messages')-->
-  @yield('content')
-  @include('includes.footer')
+  <body>
+    @include('includes.nav')
+    <!-- @include('includes.partials.messages')-->
+    <main>
+      @yield('content')
+    </main>
+    @include('includes.footer')
 
-  @stack('scripts')
-</body>
+    @stack('scripts')
+  </body>
 </html>
