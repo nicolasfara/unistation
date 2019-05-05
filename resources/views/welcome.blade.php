914f21e1fa5decdffb87a9f6da39e11ced751fd2
CavallucciMartina <martina.cavallucci96@gmail.com>
2019-05-05 14:16:03 +0200

    Add simple borders to cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
     <div class="section">
       <div class="row">
         <div class="slide-component">
-            <div class="col s12 m4 home-rule">
+            <div class="col s12 m4 home-rule" id="slide">
                 <div class="icon-block">
                     <img src="/images/logos/First.png" alt="First-logo" width="90" height="90">
                     <h5> Accedi e seleziona il tipo di pasto </h5>
@@ -24,15 +24,16 @@
             </div>
         </div>
          <div class="slide-component">
-        <div class="col s12 m4 home-rule">
+        <div class="col s12 m4 home-rule" id="slide">
           <div class="icon-block">
             <img src="/images/logos/Second.png" alt="First-logo" width="90" height="90">
             <h5>Ordina il tuo cibo preferito</h5>
+            <br>
           </div>
         </div>
         </div>
          <div class="slide-component">
-        <div class="col s12 m4 home-rule">
+        <div class="col s12 m4 home-rule" id="slide">
           <div class="icon-block">
             <img src="/images/logos/Third.png" alt="First-logo" width="90" height="90">
             <h5> Te lo porteremo in tempi record al Campus!</h5>
