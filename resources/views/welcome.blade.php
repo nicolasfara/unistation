e6d700ab5828ed5acc33a1cfc4a18b990df05497
CavallucciMartina <martina.cavallucci96@gmail.com>
2019-05-05 13:30:46 +0200

    Add text-content cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,24 +15,24 @@
         <div class="slide-component">
             <div class="col s12 m4 home-rule">
                 <div class="icon-block">
-                    <h5 class="center">Speeds up development</h5>
-                    <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                    <img src="/images/logos/First.png" alt="First-logo" width="128" height="128">
+                    <h5> Accedi e seleziona il tipo di pasto </h5>
                 </div>
             </div>
         </div>
          <div class="slide-component">
         <div class="col s12 m4 home-rule">
           <div class="icon-block">
-            <h5 class="center">User Experience Focused</h5>
-            <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+            <img src="/images/logos/Second.png" alt="First-logo" width="128" height="128">
+            <h5>Ordina il tuo cibo preferito</h5>
           </div>
         </div>
         </div>
          <div class="slide-component">
         <div class="col s12 m4 home-rule">
           <div class="icon-block">
-            <h5 class="center">Easy to work with</h5>
-            <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+            <img src="/images/logos/Third.png" alt="First-logo" width="128" height="128">
+            <h5> Te lo porteremo in tempi record al Campus!</h5>
           </div>
         </div>
         </div>
