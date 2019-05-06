3d73ce62b8d2490d0db8b6930aa27258a2acc6a6
CavallucciMartina <martina.cavallucci96@gmail.com>
2019-05-06 21:57:29 +0200

    Add fb and ig buttons

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,30 +1,33 @@
 <footer class="teal page-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col l4 s12">
-        <h5 class="white-text">Unistation</h5>
-        <h6>Il food a portata di un click </h6>
-        <p class="grey-text text-lighten-4">
-          via Roma, 3 - Cesena (FC) <br>
-        CAP 47521</p>
-      </div>
-      <div class="col l4 s12">
-        <h5 class="white-text">Seguici su</h5>
-               <p>Social-button</p>
-      </div>
-      <div class="col l4 s12">
-        <h5 class="white-text">Hai un'attività? Unisiciti a noi!</h5>
-        <ul>
-          <li><a class="grey-text text-lighten-3" href="{{ url('vendor/register') }}">Registrati</a></li>
-          <li><a class="grey-text text-lighten-3" href="{{ url('vendor/login') }}">Accedi</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class="teal lighten-1 footer-copyright">
     <div class="container">
-    © Copyright 2019 Unistation - Tutti i diritti riservati.
-    <a class="grey-text text-lighten-4 right" href="https://www.unibo.it/it">Unibo</a>
-    </div>
-  </div>
-</footer>
+        <div class="row">
+            <div class="col l4 s12">
+                <h5 class="white-text">Unistation</h5>
+                <h6>Il food a portata di un click </h6>
+                <p class="grey-text text-lighten-4">
+                    via Roma, 3 - Cesena (FC)
+                    <br> CAP 47521</p>
+                </div>
+                <div class="col l4 s12">
+                    <h5 class="white-text">Seguici su</h5>
+                    <a class="waves-effect waves-light btn-floating social facebook">
+                        <i class="fa fa-facebook"></i> Sign in with facebook</a>
+                        <a class="waves-effect waves-light btn-floating social instagram">
+                            <i class="fa fa-instagram"></i> Sign in with instagram</a>
+                        </div>
+                        <div class="col l4 s12">
+                            <h5 class="white-text">Hai un'attività? Unisiciti a noi!</h5>
+                            <ul>
+                                <li><a class="grey-text text-lighten-3" href="{{ url('vendor/register') }}">Registrati</a></li>
+                                <li><a class="grey-text text-lighten-3" href="{{ url('vendor/login') }}">Accedi</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="teal lighten-1 footer-copyright">
+                    <div class="container">
+                        © Copyright 2019 Unistation - Tutti i diritti riservati.
+                        <a class="grey-text text-lighten-4 right" href="https://www.unibo.it/it">Unibo</a>
+                    </div>
+                </div>
+            </footer>
