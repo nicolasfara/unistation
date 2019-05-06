0513469f85592ab4255f98490cbaf64e4daaf34e
CavallucciMartina <martina.cavallucci96@gmail.com>
2019-05-06 14:03:01 +0200

    Refactor materialize cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,30 +15,37 @@
  <div class="container">
     <div class="section">
       <div class="row">
-        <div class="slide-component">
-            <div class="col s12 m4 home-rule" id="slide">
-                <div class="icon-block">
-                    <img src="/images/logos/First.png" alt="First-logo" width="90" height="90">
-                    <h5> Accedi e seleziona il tipo di pasto </h5>
-                </div>
+        <div class="col s4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="/images/logos/First.png" alt="First-logo" width="80" height="80">
+                    </div>
+                    <div class="card-content">
+                        <p> Accedi e seleziona il tipo di pasto </p>
+                        <br>
+                    </div>
             </div>
         </div>
-         <div class="slide-component">
-        <div class="col s12 m4 home-rule" id="slide">
-          <div class="icon-block">
-            <img src="/images/logos/Second.png" alt="First-logo" width="90" height="90">
-            <h5>Ordina il tuo cibo preferito</h5>
-            <br>
-          </div>
+        <div class="col s4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="/images/logos/Second.png" alt="Second-logo" width="80" height="80">
+                    </div>
+                    <div class="card-content">
+                        <p> Ordina il tuo cibo preferito </p>
+                        <br>
+                    </div>
+            </div>
         </div>
-        </div>
-         <div class="slide-component">
-        <div class="col s12 m4 home-rule" id="slide">
-          <div class="icon-block">
-            <img src="/images/logos/Third.png" alt="First-logo" width="90" height="90">
-            <h5> Te lo porteremo in tempi record al Campus!</h5>
-          </div>
-        </div>
+        <div class="col s4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="/images/logos/Third.png" alt="Thirs-logo" width="80" height="80">
+                    </div>
+                    <div class="card-content">
+                        <p> Te lo porteremo in tempi record al Campus!</p>
+                    </div>
+            </div>
         </div>
       </div>
 @endsection
