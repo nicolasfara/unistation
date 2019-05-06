9f0d8a0f2caef7912162033701fc98eaec20d96d
CavallucciMartina <martina.cavallucci96@gmail.com>
2019-05-06 19:00:16 +0200

    Add footer content

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,11 +1,18 @@
 <footer class="teal page-footer">
   <div class="container">
     <div class="row">
-      <div class="col l6 s12">
-        <h5 class="white-text">Footer Content</h5>
-        <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+      <div class="col l4 s12">
+        <h5 class="white-text">Unistation</h5>
+        <h6>Il food a portata di un click </h6>
+        <p class="grey-text text-lighten-4">
+          via Roma, 3 - Cesena (FC) <br>
+        CAP 47521</p>
       </div>
-      <div class="col l4 offset-l2 s12">
+      <div class="col l4 s12">
+        <h5 class="white-text">Seguici su</h5>
+               <p>Social-button</p>
+      </div>
+      <div class="col l4 s12">
         <h5 class="white-text">Hai un'attività? Unisiciti a noi!</h5>
         <ul>
           <li><a class="grey-text text-lighten-3" href="{{ url('vendor/register') }}">Registrati</a></li>
@@ -16,7 +23,7 @@
   </div>
   <div class="teal lighten-1 footer-copyright">
     <div class="container">
-    © 2019 Copyright
+    © Copyright 2019 Unistation - Tutti i diritti riservati.
     <a class="grey-text text-lighten-4 right" href="https://www.unibo.it/it">Unibo</a>
     </div>
   </div>
