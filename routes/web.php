e02c241bcd118c8b922abde036b3ecf90a82b721
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2019-05-15 00:52:36 +0200

    Fix route

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,7 @@ Route::group(['prefix' => 'client'], function () {
   Route::get('/password/reset/{token}', 'ClientAuth\ResetPasswordController@showResetForm');
 
   Route::get('/settings', 'ClientSettingsController@showClientSettings')->name('settings');
-  Route::get('/clientorders', 'ClientOrdersController@showClientOrders')->name('clientorders');
+  Route::get('/orders', 'ClientOrdersController@showClientOrders')->name('clientorders');
   Route::get('/cart', 'CartController@showCart')->name('cart');
 });
 
