6f1500b27be431a25082e20cb61461b09613c7dc
CavallucciMartina <martina.cavallucci96@gmail.com>
2019-05-17 17:04:17 +0200

    Change cart design

--- resources/views/client/cart.blade.php
+++ resources/views/client/cart.blade.php
@@ -5,63 +5,72 @@
 @endsection
 
 @section('content')
-<div class="container">
-  <div class="row">
-    <table id="cart" class="responsive-table">
-      <thead>
-        <tr>
-          <th>Prodotto</th>
-          <th>Descrizione</th>
-          <th>Prezzo</th>
-          <th>Quantità</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr>
-          <td data-th="Prodotto">
-            <div class=""><img alt="foto" src="" class="img-responsive"/></div>
-          </td>
-          <td data-th="Descrizione">Descrizione</td>
-          <form>
-            <td data-th="Prezzo">Prezzo €</td>
-            <td data-th="Quantità">
+<div class="section no-pad-bot" id="index-banner">
+  <div class="container">
+    <div class="row">
+      <div class="section no-pad-bot" id="index-banner">
+        <div id="" class="col s12">
+          <div class="row">
+            <div class="col s12 m8">
+              <div class="card">
+               <div class="card-content" >
+                <table>
+                  <thead>
+                   <h5 class="center">Il tuo carrello</h5>
+                 </thead>
+                 <tbody>
+                  <tr>
+                    <td>Tagliatelle al ragù</td>
+                     <td data-th="Quantità">
               <form class="input_qta">
                 <input type="number" name="quantity"
                 min="1" max="100" step="1" value="1">
               </form>
             </td>
-            <td>
-              <a class="waves-effect waves-light btn "><i class="material-icons">refresh</i></a>
-            </td>
-            <td>
-              <a class="waves-effect waves-light btn "><i class="material-icons"> delete</i></a>
-            </td>
-          </form>
+                    <td>5 €</td>
+                    <td><a class="waves-effect waves-light btn "><i class="material-icons">refresh</i></a></td>
+                    <td><a class="waves-effect waves-light btn "><i class="material-icons"> delete</i></a></td>
+                  </tr>
+                </tbody>
+              </table>
 
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td> Totale parziale</td>
-          <td id="lblSubTotale">€</td>
-        </tr>
-        <tr>
-          <td> Spese di spedizione</td>
-          <td>2 €</td>
-        </tr>
-        <tr>
-          <td>Totale</td>
-          <td id="lblTotale"> € </td>
-        </tr>
-        <tr>
-            <td> <button class="btn waves-effect waves-light" type="submit" name="action">Checkout<i class="material-icons right">send</i> </button></td>
-        </tr>
-      
-      </tfoot>
 
-    </table>
-  </div>
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m4">
+          <div class="card">
+           <div class="card-content" >
+            <table>
+              <thead>
+                <h5 class="center">Riepilogo</h5>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Subtotale</td>
+                  <td>5 €</td>
+                </tr>
+                <tr>
+                  <td>Spese di spedizione</td>
+                  <td>2 €</td>
+                </tr>
+                <tr>
+                  <td>Totale</td>
+                  <td>7 €</td>
+                </tr>
+                <tr>
+                 <td> <button class="btn waves-effect waves-light" type="submit" name="action">Checkout<i class="material-icons right">send</i> </button></td>
+               </tr>
+             </tbody>
+           </table>
+         </div>
+       </div>
+     </div>
+   </div>
+ </div>
+</div>
+</div>
+</div>
 </div>
 @endsection
 @section('scripts')
