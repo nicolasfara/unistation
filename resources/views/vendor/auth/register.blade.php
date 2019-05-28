0cbfa060ce17fc25c5d8ef0c6d98f8c4d422c891
Bamarin [IT] <mauithebest@libero.it>
2019-05-28 08:53:24 +0200

    set input fields to be required

--- resources/views/vendor/auth/register.blade.php
+++ resources/views/vendor/auth/register.blade.php
@@ -13,7 +13,7 @@
         @csrf
         <div class="row">
           <div class="input-field col s12 form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-            <input name="name" id="name" type="text" class="validate">
+            <input name="name" id="name" type="text" required class="validate">
             <label for="name">Nome attività</label>
             @if ($errors->has('name'))
             <span class="help-block">
@@ -24,7 +24,7 @@
         </div>
         <div class="row">
           <div class="input-field col s12">
-            <input name="email" id="email" type="email" class="validate" value="{{ old('email') }}">
+            <input name="email" id="email" type="email" required class="validate" value="{{ old('email') }}">
             <label for="email">Email</label>
             @if ($errors->has('email'))
             <span class="help-block">
@@ -35,7 +35,7 @@
         </div>
         <div class="row">
           <div class="input-field col s12">
-            <input name="p_iva" id="p_iva" type="number" class="validate" value="{{ old('p_iva') }}">
+            <input name="p_iva" id="p_iva" type="number" required class="validate" value="{{ old('p_iva') }}">
             <label for="p_iva">P.IVA</label>
             @if ($errors->has('p_iva'))
             <span class="help-block">
@@ -46,7 +46,7 @@
         </div>
         <div class="row">
           <div class="input-field col s12">
-            <input name="password" id="password" type="password" class="validate">
+            <input name="password" id="password" type="password" required class="validate">
             <label for="password">Password</label>
             @if ($errors->has('password'))
             <span class="help-block">
@@ -57,7 +57,7 @@
         </div>
         <div class="row">
           <div class="input-field col s12">
-            <input name="password_confirmation" id="password-confirm" type="password" class="validate">
+            <input name="password_confirmation" id="password-confirm" type="password" required class="validate">
             <label for="password-confirm">Ripeti Password</label>
             @if ($errors->has('password_confirmation'))
             <span class="help-block">
