b6584ef6fd7ba9f4c44fe202c66268b0a9af07df
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2019-05-31 11:06:24 +0200

    Fix

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -2,13 +2,13 @@
 
 <div class="row container">
 
-  <div class="col s10">
+  <div class="col s8">
     <span class="cookie-consent__message">
         {!! trans('cookieConsent::texts.message') !!}
     </span>
   </div>
 
-  <div class="col s2">
+  <div class="col s4">
     <button class="js-cookie-consent-agree cookie-consent__agree btn">
         {{ trans('cookieConsent::texts.agree') }}
     </button>
