37caa61722d36ebaca881d816ca329adc56863eb
Luca Marini <luca.marini9@studio.unibo.it>
2019-05-31 11:33:25 +0200

    add rating to cards

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -26,17 +26,24 @@
                  @if($product->image != null)
                  <img src="{{ Storage::url($product->image) }}" alt="" class="activator circle responsive-img">
                  @else
-                 <img src="https://s3-eu-west-1.amazonaws.com/unistation/placeholders/imageprofile-placeholder-350x350.png" alt="" class="circle responsive-img">
+                 <img src="https://s3-eu-west-1.amazonaws.com/unistation/placeholders/imageprofile-placeholder-350x350.png" alt="" class="activator circle responsive-img">
                  @endif
                  <a id"add_btn" onclick="addToCart({{ $product->id }})" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add_shopping_cart</i></a>
                </div>
                <div class="card-content" >
-                <span class="card-title center-align">{{ $product->name }}</span>
+                <span class="card-title activator"><i class="material-icons left">expand_less</i>{{ $product->name }}</span>
                 <p class="right-align">{{ $product->price }}&euro;</p>
               </div>
               <div class="card-reveal">
                 <span class="card-title grey-text text-darken-4">{{ $product->vendor->name }}<i class="material-icons right">close</i></span>
-                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                <p class="material-icons">
+                  @for ($i = 1; $i <= $product->vendor->rating; $i++)
+                      grade
+                  @endfor
+                  @for (; $i <= 5; $i++)
+                      star_border
+                  @endfor
+                </p>
               </div>
             </div>
           </div>
