4e96a8947c2c63cac0771aa001616f725b6f60ed
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2019-06-03 18:11:42 +0200

    Remove drink (bugs in library)

--- app/Product.php
+++ app/Product.php
@@ -19,7 +19,7 @@ class Product extends Model
 
     public static function getProductsType()
     {
-        return ['appetizer', 'first', 'second', 'salad', 'dessert', 'drink'];
+        return ['appetizer', 'first', 'second', 'salad', 'dessert'];
     }
 }
 
