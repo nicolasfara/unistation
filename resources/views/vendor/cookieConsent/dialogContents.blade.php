27f1225c65b7233081841f3573a0dd07ed914d60
CavallucciMartina <martina.cavallucci96@gmail.com>
2019-06-03 23:16:38 +0200

    Fix cookie text

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,14 +1,14 @@
-<div class="js-cookie-consent cookie-consent valign-wrapper">
+<div class="js-cookie-consent cookie-consent">
 
 <div class="row container">
 
-  <div class="col s8">
-    <span class="cookie-consent__message">
+  <div class="section col s8">
+    <p class="cookie-consent__message">
         {!! trans('cookieConsent::texts.message') !!}
-    </span>
+    </p>
   </div>
 
-  <div class="col s4">
+  <div class=" button-cookie col s4 valign-wrapper">
     <button class="js-cookie-consent-agree cookie-consent__agree btn">
         {{ trans('cookieConsent::texts.agree') }}
     </button>
